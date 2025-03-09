7cfa5843bd5569c4f9bd53ac29b55939c79f88c3
May Guan <maymayy08@users.noreply.github.com>
2025-03-09 13:15:32 +0000

    .

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const sequelize = require("./config/connection");
+// const sequelize = require("./config/connection");
 const applySchema = require('./applySchema');
 applySchema();
 
